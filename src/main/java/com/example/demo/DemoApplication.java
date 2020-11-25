246cbd93e1af4c9b82f37b2da9d751486fc89e71
yupengfei <yupengfei@idstaff.com>
2020-11-26 00:41:02 +0800

    2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("第一行代码");//后面加东西
-        System.out.println("第一行代码a");
+        System.out.println("第一行代码b");
     }
 
 }
