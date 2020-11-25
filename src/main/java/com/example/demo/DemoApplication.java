e1f61d9e50f5f99e504966cf276c36a43c113377
yupengfei <yupengfei@brilliance.com.cn>
2020-11-26 00:39:26 +0800

    [git]1a

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("第一行代码");//后面加东西
+        System.out.println("第一行代码a");
     }
 
 }
